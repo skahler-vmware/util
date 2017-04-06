240d5301dd27407911af9ede67b49f79ba5a4dff
Scott Kahler <skahler@pivotal.io>
2017-04-06 12:40:22 -0500

    Update install_gpdb.sh

--- install_gpdb.sh
+++ install_gpdb.sh
@@ -12,6 +12,34 @@ ssh -oStrictHostKeyChecking=no `hostname` 'uptime'
 mkdir /home/gpadmin/gpdb_build
 chmod 777 /home/gpadmin/gpdb_build/
 
+cd /home/gpadmin/gpdb_build
+rm -rf gp-xerces
+git clone https://github.com/greenplum-db/gp-xerces.git
+cd gp-xerces
+mkdir build
+cd build
+../configure --prefix=/usr/local/gpdb
+make -j8
+make install
+
+cd /home/gpadmin/gpdb_build
+rm -rf gporca
+git clone https://github.com/greenplum-db/gporca.git
+cd gporca
+mkdir build
+cd build
+cmake3 -DCMAKE_INSTALL_PREFIX=/usr/local/gpdb ..
+make -j8
+sudo make install
+
+cd /home/gpadmin/gpdb_build
+rm -rf gpdb
+git clone https://github.com/greenplum-db/gpdb.git
+cd gpdb
+./configure --with-perl --with-python --with-libxml --enable-mapreduce --enable-orca --prefix=/usr/local/gpdb CFLAGS="-I/usr/local/gpdb/include/ -L/usr/local/gpdb/lib/"
+make -j8
+make install
+
 cat > gpinitsystem_singlenode <<EOF
 ARRAY_NAME="GPDB SINGLENODE"
 MACHINE_LIST_FILE=./hostlist
