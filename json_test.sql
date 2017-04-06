6a365f55813bc4c7f13d7b14313604aa8c9de594
Scott Kahler <skahler@pivotal.io>
2017-04-06 09:53:35 -0500

    Create json_test.sql

---
+++ json_test.sql
@@ -0,0 +1,15 @@
+CREATE TABLE cards (
+  id integer NOT NULL,
+  board_id integer NOT NULL,
+  data json
+);
+
+INSERT INTO cards VALUES (1, 1, '{"name": "Paint house", "tags": ["Improvements", "Office"], "finished": true}');
+INSERT INTO cards VALUES (2, 1, '{"name": "Wash dishes", "tags": ["Clean", "Kitchen"], "finished": false}');
+INSERT INTO cards VALUES (3, 1, '{"name": "Cook lunch", "tags": ["Cook", "Kitchen", "Tacos"], "ingredients": ["Tortillas", "Guacamole"], "finished": false}');
+INSERT INTO cards VALUES (4, 1, '{"name": "Vacuum", "tags": ["Clean", "Bedroom", "Office"], "finished": false}');
+INSERT INTO cards VALUES (5, 1, '{"name": "Hang paintings", "tags": ["Improvements", "Office"], "finished": false}');
+
+SELECT data->>'name' AS name FROM cards;
+
+SELECT * FROM cards WHERE data->>'finished' = 'true';
